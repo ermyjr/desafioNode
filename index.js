754316d8a715a7a82e23ec18b695068f59af2adf
Elizeu Junior <77870830+ermyjr@users.noreply.github.com>
2022-09-18 23:37:39 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -28,3 +28,74 @@ const checkUserId = (request, response, next) => {
     next()
 }
 
+
+app.get('/users', (request, response) => {
+    return response.json(users)
+})
+
+app.post('/users', (request, response) => {
+
+    const { name, age } = request.body
+
+    const user = { id: uuid.v4(), name, age }
+
+    users.push(user)
+
+    return response.status(201).json(user)
+})
+
+
+app.put('/users/:id', checkUserId, (request, response) => {
+
+    const { name, age } = request.body
+
+    const index = request.userIndex
+
+    const id = request.userId
+
+    const updatedUser = { id, name, age }
+
+
+
+    users[index] = updatedUser
+
+
+
+    return response.json(updatedUser)
+})
+
+app.delete('/users/:id', checkUserId, (request, response) => {
+   
+    const index = request.userIndex
+
+    users.splice(index, 1)
+
+    return response.status(204).json()
+})
+
+app.get('/orders/:id', checkUserId, requests, (request, response) => {
+    const index = request.userIndex
+    const order = orders[index]
+
+    return response.json(order)
+})
+app.patch('/orders/:id', checkUserId, requests, (request, response) => {
+    const index = request.userIndex
+    const { id, clientName, order, price } = orders[index]
+    let status = orders[index].status
+    status = "Pedido Pronto"
+    const finishedOrder = { id, order, clientName, price, status }
+    orders[index] = finishedOrder
+
+    return response.json(finishedOrder)
+})
+
+
+
+
+
+
+
+
+
+
